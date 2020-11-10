d59053f2e3db692fb65d278f961ce7d22d368b83
GERFSHARP <angiecsb982@gmail.com>
2020-11-10 16:06:27 -0500

    api

--- routes/api/teacher_eval/api.php
+++ routes/api/teacher_eval/api.php
@@ -8,14 +8,11 @@ use App\Http\Controllers\TeacherEval\SelfEvaluationController;
 use App\Http\Controllers\TeacherEval\EvaluationController;
 
 
+
 Route::apiResource('evaluation_types',EvaluationTypeController::class);
 Route::apiResource('questions', QuestionController::class);
+Route::apiResource('answers', AnswerController::class);
 
-Route::get('answers', 'App\Http\Controllers\TeacherEval\QuestionController@indexAnswer');
-Route::get('answers/{id}', 'App\Http\Controllers\TeacherEval\QuestionController@showAnswer');
-Route::post('answers', 'App\Http\Controllers\TeacherEval\QuestionController@storeAnswer');
-Route::put('answers/{id}', 'App\Http\Controllers\TeacherEval\QuestionController@updateAnswer');
-Route::delete('answers/{id}', 'App\Http\Controllers\TeacherEval\QuestionController@destroyAnswer');
 Route::apiResource('evaluations', EvaluationController::class);
 Route::apiResource('detail_evaluations', App\Http\Controllers\TeacherEval\DetailEvaluationController::class);
 Route::apiResource('student_evaluations', App\Http\Controllers\TeacherEval\StudentEvaluationController::class);
