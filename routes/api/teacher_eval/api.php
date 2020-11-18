5a7f11795b76e3e352bf811271f66d1ea9f03ebf
YadiraPavon <ylp.meneses@yavirac.edu.ec>
2020-11-18 14:11:38 -0500

    cambio

--- routes/api/teacher_eval/api.php
+++ routes/api/teacher_eval/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\TeacherEval\PairEvaluationController;
 use App\Http\Controllers\TeacherEval\SelfEvaluationController;
 use App\Http\Controllers\TeacherEval\EvaluationController;
 use App\Http\Controllers\TeacherEval\AnswerController;
+use App\Http\Controllers\TeacherEval\DetailEvaluationController;
+use App\Http\Controllers\TeacherEval\StudentEvaluationController;
 
 
 
@@ -15,8 +17,8 @@ Route::apiResource('questions', QuestionController::class);
 Route::apiResource('answers', AnswerController::class);
 
 Route::apiResource('evaluations', EvaluationController::class);
-Route::apiResource('detail_evaluations', App\Http\Controllers\TeacherEval\DetailEvaluationController::class);
-Route::apiResource('student_evaluations', App\Http\Controllers\TeacherEval\StudentEvaluationController::class);
+Route::apiResource('detail_evaluations', DetailEvaluationController::class);
+Route::apiResource('student_evaluations', StudentEvaluationController::class);
 Route::apiResource('self_evaluations', SelfEvaluationController::class);
 Route::apiResource('pair_evaluations', PairEvaluationController::class)->except(['store']);
 Route::post('pair_evaluations/teachers',[PairEvaluationController::class,'storeTeacherEvalutor']);
